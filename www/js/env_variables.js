461c9251e188ffe8233e743928471b6230da4e50
madex-sdk-developer <sdk@madex.world>
2023-12-21 16:21:38 +0300

    feat: update demo app

--- www/js/env_variables.js
+++ www/js/env_variables.js
@@ -1,12 +1,7 @@
 var EnvVariables = {
-   publisherID: "65057899-a16a-4877-989b-38c432a7fa15",
-   madexInterstitialUnitID: "b8359c60-9bde-47c9-85ff-3c7afd2bd982",
-   madexRewardedUnitID: "eaac7a7f-b0b0-46d2-ac95-bd58578e9e29",
-   madexBannerUnitID: "",
-   yandexInterstitialUnitID: "cd1dff91-76d0-44c2-a6ca-fd3f446ef9b5",
-   yandexRewardedlUnitID: "7e6334fc-ef08-45e9-9581-d18026a2fadb",
-   ironsourceInterstitialUnitID: "ec9decde-58c8-4d1b-885f-479b05f39dcb",
-   ironsourceRewardedlUnitID: "9b45ac1a-ca72-4d57-9f0f-7bab924ad1b4",
-   mintegralInterstitialUnitID: "",
-   mintegraleRewardedlUnitID: "",
+   publisherID: "d42994c6-2145-4269-9c2f-2adcf9d9703f",
+   madexInterstitialUnitID: "50f37030-162d-4e8c-a9c0-e078d8fbf2f7",
+   madexRewardedUnitID: "dacd566d-8487-4bc6-8afc-486f625be870",
+   ironsourceInterstitialUnitID: "82e3bba6-6add-4594-8bb7-f96fef5b252d",
+   ironsourceRewardedlUnitID: "42e716e1-91c0-47e0-9832-c36ada6fa836",
 }
